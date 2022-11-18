aa4e3411a47473f25dd84c1261335c36229314b4
ermz <eramirez4369@gmail.com>
2022-11-17 23:53:39 -0500

    Further worked on aave borrow script

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,5 @@
 const { getNamedAccounts, ethers } = require("hardhat")
-const { getWeth } = require("../scripts/getWeth")
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
 
 async function main() {
     // the protocol treats everything as an ERC20 token
@@ -9,17 +9,46 @@ async function main() {
 
     // Lending Pool Addresses Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
     // Lending Pool: ^
+
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
+
+    // Deposit
+    const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    // approve
+    await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited!")
 }
 
 async function getLendingPool(account) {
     const lendingPoolAddressesProvider = await ethers.getContractAt(
-        "ILendingPoolAddresses",
+        "ILendingPoolAddressesProvider",
         "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
         account
     )
 
     const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
-    // const lendingPool = await ethers.getContractAt("")
+    const lendingPool = await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        account
+    )
+
+    return lendingPool
+}
+
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt(
+        "IERC20",
+        erc20Address,
+        account
+    )
+
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved")
 }
 
 main()
